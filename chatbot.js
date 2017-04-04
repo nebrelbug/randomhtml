be762df309c3049bc9211221a483b29693c14939
nebrelbug <nebrelbug@gmail.com>
2017-04-04 14:24:42 -0600

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,1 +1,12 @@
-var life = "1"
+var userInput = document.getElementById("userInput").value;
+var response = document.getElementById("response);
+function getAnswer() {
+  if (userInput.lower === "hi") {
+    response.textContent = "Hi to you, too"
+  } else {
+    if (userInput.lower === "cookies") {
+      response.textContent = "Cake!"
+  }
+}
+getAnswer();
+
