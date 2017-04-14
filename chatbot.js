9d6ef35c5fb6e69d59ec5352c741a7918b4ed67d
nebrelbug <nebrelbug@gmail.com>
2017-04-14 14:48:25 -0600

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,32 +1,6 @@
-submitButton.addEventListener("click", onButtonClick);
-var userInputRaw = "undefined";
-var userInputLower = userInputRaw;    
+document.getElementById("submit").addEventListener("click", onButtonClick);
+var response = document.getElementById("response");
 
-var submitButton = document.getElementById("submit");
-
-function getAnswer() {
-  var response = document.getElementById("response");
-  if (userInputLower === "hi") {
-    response.innerHTML = "Hi to you, too";
-  } else {
-    if (userInputLower === "cookies") {
-      response.innerHTML = "Cake!";
-  }
+function onButtonClick() {
+  response.innerHTML = "You clicked the button!";
 }
-
-
-// 1. Find and store the element we want to listen to events on.
-
-    // 2. Define the function that will respond to the event.
-    // callback 
-    var onButtonClick = function() {
-      var userInputRaw = document.getElementById("userInput").value;
-      var userInputLower = userInputRaw.toLowerCase();
-      getAnswer();
-    };
-    // 3. Add the event listener for the element and function
-    // anonymous "inline"
-
-    
-
-
