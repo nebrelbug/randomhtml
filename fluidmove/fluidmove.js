64be6a59ccf1710b6eb4bc75e9608aa70332c03e
nebrelbug <nebrelbug@gmail.com>
2017-05-10 14:22:56 -0600

    Update fluidmove.js

--- fluidmove/fluidmove.js
+++ fluidmove/fluidmove.js
@@ -1,3 +1,79 @@
+
+var keys = [];
+    	function keyAction () {
+    if (keys[38]) { //this checks if up arrow is pressed
+        Player.yv = Player.yv - 1;
+    }
+    if (keys[40]) {
+        Player.yv = Player.yv + 1;
+    }
+    if (keys[37]) {
+        Player.xv = Player.xv - 1;
+    }
+    if (keys[39]) {
+        Player.xv = Player.xv + 1;
+    }
+}
+
+var Player = {
+xv: 0,
+yv: 0,
+xpos: 200,
+ypos: 200,
+}
+
+function movement () {
+    Player.xpos = Player.xpos + Player.xv; //setting the positions to the positions + movement
+    Player.ypos = Player.ypos + Player.yv;
+    Player.xv = Player.xv * 0.9; //slowing it down
+    Player.yv = Player.yv * 0.9;
+}
+
+function sketchProc(processing) {
+
+function drawPlayer () {
+    processing.fill(255, 255, 255);
+    processing.ellipse(Player.xpos, Player.ypos, 30, 30); //moves the circle
+    keyAction();
+    movement();
+    
+}
+
+processing.setup = function() {
+	processing.background(0,0,0);
+	processing.size(400,400);
+}
+	
+processing.draw = function() {
+    processing.background(0, 0, 0); //Background
+    drawPlayer();
+}
+}
+
+$(document).ready(function() {
+var canvas = document.getElementById("canvas");
+var processingInstance = new Processing(canvas, sketchProc);
+	
+$(document).keydown(function (e) {
+    keys[e.which] = true;
+	keyAction();
+});
+
+$(document).keyup(function (e) {
+    delete keys[e.which];
+    keyAction();
+});
+});
+
+
+
+
+
+
+
+
+
+
 void setup() {
 size(400,400);
 background(0,0,0);
